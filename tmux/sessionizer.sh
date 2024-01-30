2bf7ff74f557dc6c16e55ce534d17c459c7cf6d5
KishiBot <setnicka33@gmail.com>
2024-01-30 13:03:03 +0100

    fixed sessionizer to work with already existing sessions.. though the code might be bad but it works :>

--- tmux/sessionizer.sh
+++ tmux/sessionizer.sh
@@ -11,15 +11,20 @@ if [[ -z $selected ]]; then
 fi
 
 selected_name=$(basename "$selected" | tr . _)
-tmux_running=$(pgrep tmux)
+tmux_running=$(tmux has-session 2> /dev/null)
 
-if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    tmux new-session -s $selected_name -c $selected
+if [[ -z "$tmux_running" ]] && tmux has-session -t $selected_name 2>/dev/null; then
+    tmux attach -t $selected_name
     exit 0
-fi
+else
+    if [[ -z $TMUX ]] && [[ -z "$tmux_running" ]]; then
+        tmux new-session -s $selected_name -c $selected
+        exit 0
+    fi
 
-if ! tmux has-session -t=$selected_name 2> /dev/null; then
-    tmux new-session -ds $selected_name -c $selected
+    if ! tmux has-session -t=$selected_name 2> /dev/null; then
+        tmux new-session -ds $selected_name -c $selected
+    fi
 fi
 
 tmux switch-client -t $selected_name
